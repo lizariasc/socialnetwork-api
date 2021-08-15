a2e275c783c70c1612e0702e159ed80d9d3f353f
Liz A <lizethariasc14@gmail.com>
2021-08-15 14:18:13 -0700

    fixed errors and added dateFormate

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -8,13 +8,8 @@ const thoughtController = {
         path: "reactions",
         select: "-__v",
       })
-      .populate({
-        path: "thoughts",
-        select: "-__v",
-      })
-      .select("-__v")
       .then((dbThoughtData) => res.json(dbThoughtData))
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
@@ -37,25 +32,21 @@ const thoughtController = {
   },
   // create thought 
   createThought({ body }, res) {
-    console.log(body);
     Thought.create(body)
-      .then((thoughtData) => {
-        return User.findOneAndUpdate(
-          { _id: body.userId },
-          { $push: { thoughts: thoughtData._id } },
-          { new: true }
-        );
-      })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "No User found with this id" });
-          return;
+    .then(({ _id }) => {
+        return User.findOneAndUpdate({ _id: params.userId }, { $push: { thoughts: _id }}, { new: true });
+    })
+    .then(dbThoughtData => {
+        if(!dbThoughtData) {
+            res.status(404).json({message: 'No username found with this id!'});
+            return;
         }
-        res.json(dbUserData);
-      })
-      .catch((err) => res.json(err));
-  },
-  //update thought by it's id
+        res.json(dbThoughtData)
+    })
+    .catch(err => res.json(err)); 
+},
+
+  //update thought 
   updateThought({ params, body }, res) {
     Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .then((dbThoughtData) => {
@@ -67,6 +58,7 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
   // delete a thought
   deleteThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.id })
@@ -79,6 +71,7 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
   // add Reaction
   addReaction({ params, body }, res) {
     Thought.findOneAndUpdate(
